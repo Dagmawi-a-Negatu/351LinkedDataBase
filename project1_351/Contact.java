661dc2c209986b45a1bf2e30c66cd3906c2cf164
Connor Raebel <cmraebel1@catamount.wcu.edu>
2024-02-11 12:23:42 -0500

    Finished Contact.java and added ContactInterface

--- project1_351/Contact.java
+++ project1_351/Contact.java
@@ -1,4 +1,4 @@
-public class Contact implements ContactInterface, Clonable{
+public class Contact implements ContactInterface, Cloneable {
 	
 	private PersonalInfo personalInfo;
 	private Address address;
@@ -6,7 +6,7 @@ public class Contact implements ContactInterface, Clonable{
 	private String emailAddress;
 	
 	
-	class Address{
+	class Address {
 		
 	    private String streetAddress;
 		private String city;
@@ -19,30 +19,38 @@ public class Contact implements ContactInterface, Clonable{
 			this.city = city;
 			this.state = state;
 			this.zipCode = zipCode;
-		
-		
-			public String getstreetAddress(){
-				return (this.streetAddress);
-			}
-			
-			public String city() {
-				return (this.city);
-			}
-			
-			public String state() {
-				return (this.state);
-			}
-			
-			public int getZipCode() {
-				return (this.zipCode);
-			}
+		}
 
-        }
+		public String getstreetAddress(){
+			return (this.streetAddress);
+		}
 		
+		public String city() {
+			return (this.city);
+		}
+		
+		public String state() {
+			return (this.state);
+		}
+		
+		public int getZipCode() {
+			return (this.zipCode);
+		}
+
+		@Override
+		public Address clone() {
+			Address copy = null;
+			try {
+				copy = (Address)super.clone();
+			} catch (CloneNotSupportedException cnse){
+				System.out.println(cnse.getMessage());
+			}
+			return copy;
+		}
 	}
 	
 	
-	public Contact(int phoneNumber, String emailAddress, Address, address,
+	public Contact(int phoneNumber, String emailAddress, Address address,
     PersonalInfo personalInfo) {
 		
 		this.phoneNumber = phoneNumber;
@@ -66,7 +74,7 @@ public class Contact implements ContactInterface, Clonable{
         Contact theCopy = null;
         try {
             theCopy = (Contact)super.clone(); // clone Contact's fields
-            theCopy.address = (Address) address.clone;
+            theCopy.address = (Address) address.clone();
         }
         catch(CloneNotSupportedException cnse) {
             System.out.println(cnse.getMessage());
@@ -76,13 +84,6 @@ public class Contact implements ContactInterface, Clonable{
 
     @Override
 	public boolean exists(String attriubte) {
-
-		List<Field> attributes = getAllFields(this);
-		for (Field field : attributes) {
-			if (attriubte.equalsIgnoreCase(field)) {
-				return true;
-			}
-        }
 		try {
 			this.getClass().getDeclaredField(attriubte);
 			return true;
@@ -96,7 +97,6 @@ public class Contact implements ContactInterface, Clonable{
 	 * Check to see if a record has an attribute containing a specific value.
 	 * Should not be case sensitive.
 	 * @param attribute possible attribute within a record.
-	 * @param value desire value of the attribute.
 	 * @param value desired value of the attribute.
 	 * @return true if the value is contained in the object, false otherwise.
 	 * @throws IllegalArgumentExcpetion if the attirubte is invalid.
@@ -104,49 +104,36 @@ public class Contact implements ContactInterface, Clonable{
 	@Override
 	public boolean hasValue(String attribute, String value)
     throws IllegalArgumentException {
-		List<Field> attributes = getAllFields(this);
-		for (Field field : attributes) {
-			if (attriubte.equalsIgnoreCase(field)
-                && field.get(this).equalsIgnoreCase(value)) {
-		  		return true;
-            }
-        }
 		if (!exists(attribute)) {
 			throw new IllegalArgumentException();
 		} else {
 			try {
-				    String val = (String)this.getClass().getDeclaredField
-                    (attribute).get(this);
-				    if (val.equalsIgnoreCase(value)) {
-					    return true;
-				    } else {
-					    return false;
-				    }
-			    } catch (NoSuchFieldException nsfe) {
-				    System.out.println(nsfe.getMessage());
-				    return false;
-			    } catch (IllegalAccessException iae) {
-				    System.out.println(iae.getMessage());
-				    return false;
-			    }
-		  }
-		return false;
+				String val = (String)this.getClass().getDeclaredField
+				(attribute).get(this);
+				if (val.equalsIgnoreCase(value)) {
+					return true;
+				} else {
+					return false;
+				}
+			} catch (NoSuchFieldException nsfe) {
+				System.out.println(nsfe.getMessage());
+				return false;
+			} catch (IllegalAccessException iae) {
+				System.out.println(iae.getMessage());
+				return false;
+			}
+		}
     }
 
-   }
 	@Override
 	public void setValue(String attribute, String value) 
     throws IllegalArgumentException {
-		// TODO Auto-generated method stub
-
-		if(!exists(attribute)) {
+		if (!exists(attribute)) {
 			throw new IllegalArgumentException();
         }
 	    else {
 			try {
-				this.getClass().getDeclaredField(attribute)
-                field.setAccessible(true);
-                field.set(this, value);
+				this.getClass().getDeclaredField(attribute).set(this, value);
 			} catch (NoSuchFieldException nsfe) {
 				System.out.println(nsfe.getMessage());
 			} catch (IllegalAccessException iae) {
@@ -154,5 +141,4 @@ public class Contact implements ContactInterface, Clonable{
 			}
 		}
     }
-
 }
