76bd9fc1141e49bf3e4047a2b199b38287aeddf7
Connor Raebel <cmraebel1@catamount.wcu.edu>
2024-02-11 12:55:22 -0500

    Fixed errors in WorkContact

--- project1_351/WorkContact.java
+++ project1_351/WorkContact.java
@@ -1,35 +1,33 @@
-public class WorkContact extends Contact, Clonable {
+public class WorkContact extends Contact {
 	
-   private String title;
-   private String company;
-   private String department;
+    private String title;
+    private String company;
+    private String department;
 
-   public workContact(int phoneNumber, String emailAdress,
-   Address address, PersonalInfo personalInfo, String title,
-   String company, String department){
-       super(phoneNumber, emailAddress, address, personalInfo);
-       this.title = title;
-       this.company = company;
-       this.department = department;
-   }
+    public WorkContact(int phoneNumber, String emailAddress, Contact.Address address, PersonalInfo personalInfo) {
+        super(phoneNumber, emailAddress, address, personalInfo);
+    }
 
-
+    public WorkContact(int phoneNumber, String emailAddress, Contact.Address address, PersonalInfo personalInfo, String title, String company, String department) {
+        super(phoneNumber, emailAddress, address, personalInfo);
+        this.title = title;
+        this.company = company;
+        this.department = department;
+    }
    
-  @Overide
+  @Override
   public WorkContact clone() {
         WorkContact theCopy = null;
         try {
-            theCopy = (Contact)super.clone(); // clone Contact's fields
+            theCopy = (WorkContact)super.clone(); // clone Contact's fields
             theCopy.company = this.company;
             theCopy.department = this.department;
             theCopy.title = this.title;
         }
-        catch(CloneNotSupportedException cnse) {
-            System.out.println(cnse.getMessage());
+        catch(Exception e) {
+            System.out.println(e.getMessage());
         }
 
         return theCopy;
   }
-  	
-
 }
