df35d78d3a308457530362e059bc2d1d58831f49
Dagmawi-a-Negatu <117816205+Dagmawi-a-Negatu@users.noreply.github.com>
2024-02-12 10:00:12 -0500

    Update WorkContact.java

--- project1_351/WorkContact.java
+++ project1_351/WorkContact.java
@@ -2,22 +2,37 @@ package project1CS351;
 
 public class WorkContact extends Contact {
 	
-    private String title;
+    
+	private String title;
     private String company;
     private String department;
 
-    public WorkContact(Long phoneNumber, String emailAddress, Address address, PersonalInfo personalInfo) {
-        super(phoneNumber, emailAddress, address, personalInfo);
-    }
-
+    
+    
     public WorkContact(Long phoneNumber, String emailAddress,
     		Address address, PersonalInfo personalInfo, String title, String company, String department) {
         super(phoneNumber, emailAddress, address, personalInfo);
+     
         this.title = title;
         this.company = company;
         this.department = department;
     }
+    
+    
+    public String getTitle() {
+    	return (this.title);
+    }
+    
+    public String getCompany() {
+    	return (this.company);
+    }
+    
+    public String getDepartment() {
+    	return (this.department);
+    }
    
+  
+    
   @Override
   public WorkContact clone() {
         WorkContact theCopy = null;
