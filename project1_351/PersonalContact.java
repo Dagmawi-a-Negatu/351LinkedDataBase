1d6750764896d1d3113ae59c9e10056ed4bd0566
Dagmawi-a-Negatu <117816205+Dagmawi-a-Negatu@users.noreply.github.com>
2024-02-12 05:30:27 -0500

    Update PersonalContact.java

--- project1_351/PersonalContact.java
+++ project1_351/PersonalContact.java
@@ -1,19 +1,12 @@
-public class PersonalContact extends Contact implements Cloneable {
+package project1CS351;
+
+public class PersonalContact extends Contact implements Clonable {
 	
 	
     private Label label;
-	
-	enum Label {
-        MOM,
-		DAD,
-		BROTHER,
-		SISTER,
-		FRIEND, 
-		HUSBAND,
-		WIFE
-	}
-	
-	public PersonalContact(int phoneNumber, String emailAddress,
+    
+    
+	public PersonalContact(Long phoneNumber, String emailAddress,
     Address address, PersonalInfo personalInfo, Label label) {
 	    super(phoneNumber, emailAddress, address, personalInfo);
         this.label = label;
@@ -23,15 +16,14 @@ public class PersonalContact extends Contact implements Cloneable {
     @Override
     public PersonalContact clone() {
         PersonalContact theCopy = null;
-        try {
-            theCopy = (Contact)super.clone(); // clone Contact's fields
-		theCopy.label = (Lable) label.clone;
-        }
-        catch(CloneNotSupportedException cnse) {
-            System.out.println(cnse.getMessage());
-        }
+        theCopy = (PersonalContact)super.clone(); // clone Contact's fields
+		theCopy.label = (Label) this.label;
         return theCopy;
     }
+    
+  
+    
+    
+    
 
 }
-
