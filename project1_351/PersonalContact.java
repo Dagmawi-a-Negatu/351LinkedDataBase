42799a75b0318e113e18ef57f4d11047c7162e43
Dagmawi-a-Negatu <117816205+Dagmawi-a-Negatu@users.noreply.github.com>
2024-02-13 21:55:30 -0500

    Update PersonalContact.java

--- project1_351/PersonalContact.java
+++ project1_351/PersonalContact.java
@@ -1,34 +1,39 @@
 package project1CS351;
 
-public class PersonalContact extends Contact implements Clonable {
-	
-	
+public class PersonalContact extends Contact implements Cloneable { 
+    
     private Label label;
     
-    
-    
-	public PersonalContact(Long phoneNumber, String emailAddress,
-    Address address, PersonalInfo personalInfo, Label label) {
-	    super(phoneNumber, emailAddress, address, personalInfo);
+    public PersonalContact(Long phoneNumber, String emailAddress, Address address, PersonalInfo personalInfo, Label label) {
+        super(phoneNumber, emailAddress, address, personalInfo);
         this.label = label;
-		// TODO Auto-generated constructor stub
-	}
-	
-	public Label getLabel() {
-		return (this.label);
-	}
+    }
+    
+    public Label getLabel() {
+        return this.label;
+    }
 
     @Override
     public PersonalContact clone() {
         PersonalContact theCopy = null;
-        theCopy = (PersonalContact)super.clone(); // clone Contact's fields
-		theCopy.label = (Label) this.label;
+        theCopy = (PersonalContact) super.clone(); // This clones the Contact part of PersonalContact
+		// Since Label is an enum and thus inherently immutable and shared, we can simply assign it
+		theCopy.label = this.label; // No need to clone, as enum values are static and immutable
         return theCopy;
     }
+
     
+    @Override
+    public String toString() {
+        return "Catagory: " + this.getLabel() + "\n" + super.toString();
+    }
+    
+    
+}
+
   
     
     
     
 
-}
+
