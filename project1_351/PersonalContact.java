68fe0174be9dfefb8e5a0b5b41ce8c6774d08d07
Connor Raebel <cmraebel1@catamount.wcu.edu>
2024-02-14 17:37:24 -0500

    Documentation for PersonalContact and Personalinfo

--- project1_351/PersonalContact.java
+++ project1_351/PersonalContact.java
@@ -1,18 +1,32 @@
+/*
+ * The purpose of this class is to represent the information for a Personal Contact.
+ */
+
 package project1CS_351;
 
 public class PersonalContact extends Contact implements Cloneable { 
     
+    // Label field for the contact
     private Label label;
     
+    // Personal Contact constructor
     public PersonalContact(Long phoneNumber, String emailAddress, Address address, PersonalInfo personalInfo, Label label) {
         super(phoneNumber, emailAddress, address, personalInfo);
         this.label = label;
     }
     
+    /**
+     * Gets the label of the contact
+     * @return the label of the contact
+     */
     public Label getLabel() {
         return this.label;
     }
 
+    /**
+     * Clone method for the personal contact
+     * @return a clone of the personal contact
+     */
     @Override
     public PersonalContact clone() {
         PersonalContact theCopy = null;
@@ -22,7 +36,10 @@ public class PersonalContact extends Contact implements Cloneable {
         return theCopy;
     }
 
-    
+    /**
+     * To string method for the contact. 
+     * @return the information of the contact as a formatted string.
+     */
     @Override
     public String toString() {
         return "Catagory: " + this.getLabel() + "\n" + super.toString();
--- project1_351/PersonalInfo.java
+++ project1_351/PersonalInfo.java
@@ -1,43 +1,80 @@
+/*
+ * The purpose of this class is to represent the basic information for a person.
+ */
+
 package project1CS_351;
 
 public class PersonalInfo {
-	private String firstName; //First name
-	private String lastName;  //Last name
+
+	// First Name
+	private String firstName;
+
+	// Last Name
+	private String lastName;
+
+	// Marital Status
 	private Status martialStatus;
 	
-	
+	// Constructor with first and last name, and marital status
 	public PersonalInfo(String last, String first, Status status) {
 		this.firstName = first;
 		this.lastName = last;
 		this.martialStatus = status;
 	}
 	
+	// Constructor with only first and last name
 	public PersonalInfo(String last, String first) {
 		this.firstName = first;
 		this.lastName = last;
 	}
 	
-	public String getFristName() {
+	/**
+	 * Get the first name of the person
+	 * @return the person's first name
+	 */
+	public String getFirstName() {
 		return (this.firstName);
 	}
 	
+	/**
+	 * Get the last name of the person
+	 * @return the person's last name
+	 */
 	public String getLastName() {
 		return (this.lastName);
 	}
 	
-	public String setFristName(String firstName) {
+	/**
+	 * Set the first name of the person
+	 * @param firstName Value of the new first name
+	 * @return the new first name
+	 */
+	public String setFirstName(String firstName) {
 		return (this.firstName = firstName);
 	}
 	
+	/**
+	 * Set the last name of the person
+	 * @param lastName Value of the new last name
+	 * @return the new last name
+	 */
 	public String setLastName(String lastName) {
 		return (this.lastName = lastName);
 	}
 	
+	/**
+	 * Get the marital status of the person
+	 * @return the person's marital status
+	 */
 	public Status getMartialStatus() {
 		return (this.martialStatus);
 	}
 	
-	
+	/**
+	 * Compares this object to another personalInfo object for equality
+	 * @param personalInfo the object to be compared
+	 * @return true if equal, else false
+	 */
 	public boolean compare(PersonalInfo personalInfo) {
 	    if (this == personalInfo) {
 	    	return true; // Check for reference equality
@@ -58,7 +95,10 @@ public class PersonalInfo {
 	}
 
 
-	
+	/**
+	 * To string method for the person's info.
+	 * @returns info as a formatted string.
+	 */
 	@Override
 	public String toString() {
 		
